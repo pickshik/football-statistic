73c16a7cec165711a672be9caf2a47bd7f460b6a
DbIHbKA <dbihbka@xakep.ru>
2012-08-14 12:14:10 +0400

    Finish write function math_history

--- graber.py
+++ graber.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import urllib
 from grab import Grab
 
@@ -14,11 +15,30 @@ def match_history(first_team, second_team, n_last_match=5):
     g = Grab(log_file="/tmp/graber.log")
     g.go(URI)
     if g.search(u'Ранее не встречались'):
-        print 'fail'
+        print('fail')
     else:
-        print 'ok'
+        try:
+            all_table = g.xpath('/html/body/table[4]/tr/td[2]/table')
+        except IndexError:
+            print('fail')
+        answer = []
+        for y in all_table:
+            a_temp = []
+            for x in y:
+                if x.text == None:
+                    temp = x.xpath('a')
+                    if len(temp) > 0 and temp[0].text != None:
+                        a_temp.append(temp[0].text)
+                else:
+                    a_temp.append(x.text)
+            answer.append(a_temp)
+    return answer
 
 
 if __name__ == "__main__":
-    match_history(u'Зенит', u'Спартак')
-    match_history(u'Реал', u'Анжи')
+    for y in match_history(u'Зенит', u'Спартак'):
+        for x in y:
+            print(x, end=' | ')
+        print()
+
+    #match_history(u'Реал', u'Анжи')
